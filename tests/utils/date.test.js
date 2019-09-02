76883e8bcac8a27a717f58ec02cb34327a692d8a
Shutsen <vancampenhoutgeert@gmail.com>
2019-09-02 17:00:09 +0200

    Add date functions + tests

--- tests/utils/date.test.js
+++ tests/utils/date.test.js
@@ -1,8 +1,26 @@
-const { getYearMonthDay } = require('../../utils/date')
+const { getDayMonthYearString, getTodayAndTomorrowObject } = require('../../utils/date')
 
 test('Should output the correct date', () => {
 	let d = new Date("August 9, 1986 09:08:00");
-	let date = getYearMonthDay(d)
+	let date = getDayMonthYearString(d)
 
 	expect(date).toBe('09/08/1986')
+})
+
+test('Should output the correct date', () => {
+	let d = new Date("August 9, 1986 09:08:00");
+	let date = getTodayAndTomorrowObject(d)
+
+	expect(date).toEqual({
+		"today": {
+			"day": "09",
+			"month": "08",
+			"year": 1986
+		}, 
+		"tomorrow": {
+			"day": 10,
+			"month": "08",
+			"year": 1986
+		}
+	})
 })
--- utils/date.js
+++ utils/date.js
@@ -1,14 +1,39 @@
-const getYearMonthDay = (dateObject = new Date()) => {
-	let day = dateObject.getDate()
-	let month = dateObject.getMonth() + 1
-	const year = dateObject.getFullYear()
-	
-	if (day < 10) day = '0' + day
-	if (month < 10) month = '0' + month
-
+const getDayMonthYearString = (dateObject = new Date()) => {
+	const { day, month, year } = getYearMonthDayObject(dateObject)
 	return `${day}/${month}/${year}`
 }
 
+const getYearMonthDayObject = (dateObject = new Date()) => {
+	let day = dateObject.getDate()
+	let month = dateObject.getMonth() + 1
+	const year = dateObject.getFullYear()
+
+	if (day < 10) day = '0' + day
+	if (month < 10) month = '0' + month
+
+	return { day, month, year }
+}
+
+const getTodayAndTomorrowObject = (dateObject = new Date()) => {
+	const { day, month, year } = getYearMonthDayObject(dateObject)
+
+	dateObject.setDate(dateObject.getDate() + 1)
+
+	let tomorrow_day = dateObject.getDate()
+	let tomorrow_month = dateObject.getMonth() + 1
+	const tomorrow_year = dateObject.getFullYear()
+
+	if (tomorrow_day < 10) tomorrow_day = '0' + tomorrow_day
+	if (tomorrow_month < 10) tomorrow_month = '0' + tomorrow_month
+
+	return {
+		today: { day, month, year },
+		tomorrow: { day: tomorrow_day, month: tomorrow_month, year: tomorrow_year }
+	}
+}
+
 module.exports = {
-	getYearMonthDay
+	getDayMonthYearString,
+	getYearMonthDayObject,
+	getTodayAndTomorrowObject
 }
